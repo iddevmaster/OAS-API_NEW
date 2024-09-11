b317ca6a561aaf986e2b7aebc22535b62454bef5
iboonkhet <iboonkhet@gmail.com>
2024-09-11 17:21:45 +0700

    Update database.js

--- database.js
+++ database.js
@@ -16,7 +16,7 @@ const pool = mysql.createPool({
   port: "3306",
   // password: "",
   password: "@P@SS.W0rd",
-  database: "oas",
+  database: "oas_uat",
   waitForConnections: true,
   // connectionLimit: 100,
   queueLimit: 0,
