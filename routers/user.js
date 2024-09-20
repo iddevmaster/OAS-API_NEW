ec628cc128f5c44f8dc634c7b66c37f041703ab3
iboonkhet <iboonkhet@gmail.com>
2024-09-20 13:45:06 +0700

    Update user.js

--- routers/user.js
+++ routers/user.js
@@ -629,9 +629,11 @@ router.post("/updatedata/log", middleware, async (req, res, next) => {
   const data = req.body;
 
   let result = await runQuery(
-    "INSERT INTO app_log_update_data (user_id,update_data,udp_date) VALUES (?,?,?)",
+    "INSERT INTO app_log_update_data (user_id,create_id,des,update_data,udp_date) VALUES (?,?,?,?,?)",
     [
       data.user_id,
+      data.user_admin,
+      data.des,
       functions.dateAsiaThai(),
       functions.dateAsiaThai(),
     ]
