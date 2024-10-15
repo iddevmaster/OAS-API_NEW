cea525007292adcf5cc8bc4e760db56a100bf3b4
iboonkhet <iboonkhet@gmail.com>
2024-10-15 16:57:55 +0700

    Update appointment.js

--- routers/appointment.js
+++ routers/appointment.js
@@ -564,10 +564,11 @@ router.post("/dateappointment", middleware, async (req, res, next) => {
 
 obj = [];
 
-let sql = `SELECT A.*,(SELECT mr_score FROM app_main_result WHERE user_id= A.user_id AND mr_learn_type = 1) AS thero,(SELECT mr_score FROM app_main_result WHERE user_id= A.user_id AND mr_learn_type = 2) AS pratic,A.user_id,B.dlt_code,C.user_firstname,C.user_lastname,C.user_prefrix,C.user_full_name from app_appointment_reserve A
+let sql = `SELECT A.*,(SELECT mr_score FROM app_main_result WHERE user_id= A.user_id AND mr_learn_type = 1) AS thero,(SELECT mr_score FROM app_main_result WHERE user_id= A.user_id AND mr_learn_type = 2) AS pratic,A.user_id,B.dlt_code,C.user_firstname,C.user_lastname,C.user_prefrix,C.user_full_name,B.type from app_appointment_reserve A
 LEFT JOIN app_appointment B ON A.ap_id = B.ap_id
 LEFT JOIN app_user C ON C.user_id = A.user_id
-WHERE DATE(B.ap_date_first) = '2024-10-14'`;
+WHERE DATE(B.ap_date_first) = '2024-10-14'
+`;
 let getContent = await runQuery(sql);
   return res.json(getContent);
 });
