7636c94d4ba7e6ba383a1b98194fcc44aac8386f
iboonkhet <itservices@iddrives.co.th>
2024-10-22 11:49:53 +0700

    Update appointment.js

--- routers/appointment.js
+++ routers/appointment.js
@@ -642,10 +642,10 @@ router.post("/dateappointment/appbyuser", middleware, async (req, res, next) => 
         "SELECT COUNT(*) as numRows from app_appointment_reserve A LEFT JOIN app_appointment B ON A.ap_id = B.ap_id WHERE A.ap_id =? AND A.st_id IS NOT NULL",
         [data.ap_id]
       );
-      return res.json(getContent[0]?.numRows);
+      return res.json(getContent[0]?.numRows + 1);
     }
     if(data.stat == 'same'){
-      res.json(data.stat);
+      return res.json(0);
     }
 
   
