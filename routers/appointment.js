010f8f372e3b91acd762c0e592e061d4bbb0f5e1
Rkknoob <iboonkhet@gmail.com>
2024-10-29 20:09:10 +0700

    Update appointment.js

--- routers/appointment.js
+++ routers/appointment.js
@@ -573,40 +573,44 @@ router.post("/dateappointment", middleware, async (req, res, next) => {
 
 
 
-      let sql = `select * from app_zipcode_lao where id = ? `;
+ 
 
-  let getContent = await runQuery(sql,[location]);
-  return res.json(getContent[0].province_code);
-//   if(user_type == 1){
+  if(user_type == 1){
 
 
-//     let sql = `SELECT D.*,A.*,(SELECT mr_score FROM app_main_result WHERE user_id= A.user_id AND mr_learn_type = 1) AS thero,(SELECT mr_score FROM app_main_result WHERE user_id= A.user_id AND mr_learn_type = 2) AS pratic,(select mr_status from app_main_result  WHERE user_id= A.user_id AND mr_learn_type = 1) AS mr_status_t,(select mr_status from app_main_result  WHERE user_id= A.user_id AND mr_learn_type = 2) AS mr_status_p,A.user_id,B.dlt_code,C.user_firstname,C.user_lastname,C.user_prefrix,C.user_full_name,B.type,D.user_img from app_appointment_reserve A
-//     LEFT JOIN app_appointment B ON A.ap_id = B.ap_id
-//     LEFT JOIN app_user C ON C.user_id = A.user_id
-//     LEFT JOIN app_user_detail D ON C.user_id = D.user_id
-//     WHERE DATE(B.ap_date_first) = ?
-//     `;
-//     let getContent = await runQuery(sql,[ap_date_start]);
+    let sql = `SELECT D.*,A.*,(SELECT mr_score FROM app_main_result WHERE user_id= A.user_id AND mr_learn_type = 1) AS thero,(SELECT mr_score FROM app_main_result WHERE user_id= A.user_id AND mr_learn_type = 2) AS pratic,(select mr_status from app_main_result  WHERE user_id= A.user_id AND mr_learn_type = 1) AS mr_status_t,(select mr_status from app_main_result  WHERE user_id= A.user_id AND mr_learn_type = 2) AS mr_status_p,A.user_id,B.dlt_code,C.user_firstname,C.user_lastname,C.user_prefrix,C.user_full_name,B.type,D.user_img from app_appointment_reserve A
+    LEFT JOIN app_appointment B ON A.ap_id = B.ap_id
+    LEFT JOIN app_user C ON C.user_id = A.user_id
+    LEFT JOIN app_user_detail D ON C.user_id = D.user_id
+    WHERE DATE(B.ap_date_first) = ?
+    `;
+    let getContent = await runQuery(sql,[ap_date_start]);
 
-//       return res.json(getContent);
+      return res.json(getContent);
     
 
-//   }
+  }
 
-//   if(user_type == 2){
-//     let sql = `SELECT D.*,A.*,(SELECT mr_score FROM app_main_result WHERE user_id= A.user_id AND mr_learn_type = 1) AS thero,(SELECT mr_score FROM app_main_result WHERE user_id= A.user_id AND mr_learn_type = 2) AS pratic,(select mr_status from app_main_result  WHERE user_id= A.user_id AND mr_learn_type = 1) AS mr_status_t,(select mr_status from app_main_result  WHERE user_id= A.user_id AND mr_learn_type = 2) AS mr_status_p,A.user_id,B.dlt_code,C.user_firstname,C.user_lastname,C.user_prefrix,C.user_full_name,B.type,D.user_img from app_appointment_reserve A
-// LEFT JOIN app_appointment B ON A.ap_id = B.ap_id
-// LEFT JOIN app_user C ON C.user_id = A.user_id
-// LEFT JOIN app_user_detail D ON C.user_id = D.user_id
-// WHERE DATE(B.ap_date_first) = ? AND D.location_id = ?
-// `;
-// let getContent = await runQuery(sql,[ap_date_start,location]);
+  if(user_type == 2){
+
+    let sqls = `select * from app_zipcode_lao where id = ? `;
+
+    let province_code = await runQuery(sqls,[location]);
+
+    let sql = `SELECT E.province_code,D.*,A.*,(SELECT mr_score FROM app_main_result WHERE user_id= A.user_id AND mr_learn_type = 1) AS thero,(SELECT mr_score FROM app_main_result WHERE user_id= A.user_id AND mr_learn_type = 2) AS pratic,(select mr_status from app_main_result  WHERE user_id= A.user_id AND mr_learn_type = 1) AS mr_status_t,(select mr_status from app_main_result  WHERE user_id= A.user_id AND mr_learn_type = 2) AS mr_status_p,A.user_id,B.dlt_code,C.user_firstname,C.user_lastname,C.user_prefrix,C.user_full_name,B.type,D.user_img from app_appointment_reserve A
+LEFT JOIN app_appointment B ON A.ap_id = B.ap_id
+LEFT JOIN app_user C ON C.user_id = A.user_id
+LEFT JOIN app_user_detail D ON C.user_id = D.user_id
+LEFT JOIN app_zipcode_lao E ON E.id = D.location_id
+WHERE DATE(B.ap_date_first) = ? AND E.id = ?
+`;
+let getContent = await runQuery(sql,[ap_date_start,province_code]);
 
 
 
-//   return res.json(getContent);
+  return res.json(getContent);
 
-//   }
+  }
 
 
 });
