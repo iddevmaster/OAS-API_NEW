35b144d221e4717cbfaec8b42d899c1b5e3f366f
iboonkhet <itservices@iddrives.co.th>
2025-03-17 16:33:21 +0700

    update
    update

--- routers/appointment.js
+++ routers/appointment.js
@@ -135,11 +135,15 @@ let _check_user = await runQuery(
         [currentDate.toISOString().split('T')[0],group_id]
       );
 
+      let sqlx = "SELECT A.group_id,A.group,A.users_id,B.name,B.province_code,C.province_name FROM app_group_users A LEFT JOIN app_group B ON A.group = B.group_id LEFT JOIN app_zipcode_lao C ON C.province_code = B.province_code WHERE A.users_id = ? GROUP BY A.group_id,A.users_id";
+
+      let results = await runQuery(sqlx,data.user_id);
+      
   
       if(getContent[0]?.numRows == 0){
         let result = await runQuery(
         "INSERT INTO app_appointment (ap_learn_type,ap_quota,ap_date_start,ap_date_end,ap_date_first,ap_remark,dlt_code,crt_date,udp_date,user_udp,user_crt,time,group_id,day,peop_addrs) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-        [2, data.ap_quota,data.ap_date_start,data.ap_date_end,currentDate.toISOString().split('T')[0],'-','-',functions.dateAsiaThai(),functions.dateAsiaThai(), user_id,user_id,time,group_id,LoaDay.days,peop_addrs]
+        [2, data.ap_quota,data.ap_date_start,data.ap_date_end,currentDate.toISOString().split('T')[0],'-','-',functions.dateAsiaThai(),functions.dateAsiaThai(), user_id,user_id,time,group_id,LoaDay.days,results[0].province_name]
         
       )
       let ap_id = result.insertId;
